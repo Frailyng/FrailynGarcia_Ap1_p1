5ea54e8acd14203ad9b13cca5587fb7287169224
frailyng <frailindes@gmail.com>
2024-09-23 19:02:23 -0400

    Contexto Agregado

---
+++ DAL/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using FrailynGarcia_Ap1_p1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FrailynGarcia_Ap1_p1.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+        }
+
+        public DbSet<Registros> Registros { get; set; }
+    }
+}
