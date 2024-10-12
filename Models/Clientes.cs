56492f71df5c8470484a55dabbab8ecaa41aedc8
frailyng <frailindes@gmail.com>
2024-10-11 23:13:46 -0400

    Clientes Modificado

--- Models/Clientes.cs
+++ Models/Clientes.cs
@@ -1,6 +1,22 @@
-﻿namespace FrailynGarcia_Ap1_p1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FrailynGarcia_Ap1_p1.Models;
+
+
+public class Clientes
 {
-    public class Clientes
-    {
-    }
+    [Key]
+    public int ClienteId { get; set; }
+
+    [Required(ErrorMessage = "El campo Nombres No puede Estar en Blanco")]
+
+    public string Nombres { get; set; }
+    [Required(ErrorMessage = "El Rnc No puede Estar en blanco")]
+
+    public string Rnc {  get; set; }
+
+    public string Direccion { get; set; }
+
+    public int LimiteCredito { get; set; }
+
 }
