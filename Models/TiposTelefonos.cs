a9c06472fe2bb1ea02df892dd2a2f430c94150f0
frailyng <frailindes@gmail.com>
2024-10-11 23:22:39 -0400

    Tipos Telefonos Creado

---
+++ Models/TiposTelefonos.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FrailynGarcia_Ap1_p1.Models;
+
+public class TiposTelefonos
+{
+    [Key]
+    public int TipoId { get; set; }
+
+    public string Descripcion {  get; set; }
+}
