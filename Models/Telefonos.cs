ee46927956f1012157d28aeaf0527383db2ef7b2
frailyng <frailindes@gmail.com>
2024-10-12 14:54:31 -0400

    Telefonos Modificado

--- Models/Telefonos.cs
+++ Models/Telefonos.cs
@@ -1,12 +1,15 @@
-﻿namespace FrailynGarcia_Ap1_p1.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FrailynGarcia_Ap1_p1.Models
 {
     public class Telefonos
     {
-        public int TelefonoId { get; set; }        
-        public int ClienteId { get; set; }         
-        public string TipoTelefono { get; set; }  
-        public string NumeroTelefono { get; set; } 
+        public int TelefonoId { get; set; }
 
+        [ForeignKey("Cliente")]
+        public int ClienteId { get; set; }
+        public string TipoTelefono { get; set; }
+        public string NumeroTelefono { get; set; }
 
         public Clientes Cliente { get; set; }
     }
