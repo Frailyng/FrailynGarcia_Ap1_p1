8efa337f9f97f521d13fe0e06df2e4ba34337672
frailyng <frailindes@gmail.com>
2024-10-15 17:16:36 -0400

    Modelo Deudores Modificado

--- Models/Deudores.cs
+++ Models/Deudores.cs
@@ -2,13 +2,17 @@
 
 namespace FrailynGarcia_Ap1_p1.Models
 {
-    public class Deudores
+    public partial class Deudores
     {
         [Key]
         public int DeudorId { get; set; }
         [Required(ErrorMessage ="El campo Nombre es obligatorio")]
 
         public string Nombres { get; set; }
+
+        public virtual ICollection<Cobros> Cobros { get; set; } = new List<Cobros>();
+
+        public virtual ICollection<Prestamos> Prestamos { get; set;} = new List<Prestamos>();
     
        
 
