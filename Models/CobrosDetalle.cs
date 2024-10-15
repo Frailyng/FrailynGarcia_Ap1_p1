3fe2a430525c9a46d791732134d16418087f3ef5
frailyng <frailindes@gmail.com>
2024-10-15 17:02:56 -0400

    CobrosDetalle Modificado

--- Models/CobrosDetalle.cs
+++ Models/CobrosDetalle.cs
@@ -9,21 +9,15 @@ namespace FrailynGarcia_Ap1_p1.Models
         [Key]
         public int DetalleId { get; set; }
 
-        [ForeignKey("Cobro")]
         public int CobroId { get; set; }
 
-        [ForeignKey("Prestamo")]
-        public int PrestamoId { get; set; }
-
-        [ForeignKey("Deudor")]
         public int DeudorId { get; set; }
 
         public int MontoPagado { get; set; }
 
-        public Cobros Cobro { get; set; }
-        public Prestamos Prestamo { get; set; }
-
-        public Deudores Deudor  { get; set; }
+        [ForeignKey("CobroId")]
+        public virtual Cobros Cobro { get; set; } = null!;
+  
     }
 
 }
