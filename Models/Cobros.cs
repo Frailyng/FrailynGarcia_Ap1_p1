2fd8bec926fe4b373f101d339b173228bd82e935
frailyng <frailindes@gmail.com>
2024-10-26 12:24:01 -0400

    Modelo de Cobros Actualizado

--- Models/Cobros.cs
+++ Models/Cobros.cs
@@ -8,17 +8,17 @@ public class Cobros
     [Key]
     public int CobroId { get; set; }
 
-    [Required(ErrorMessage = "La fecha no puede estar en blanco")]
     public DateTime Fecha { get; set; }
 
     public int DeudorId { get; set; }
 
-    [Range(1, double.MaxValue, ErrorMessage = "El monto no es valido")]
-
+    [Range(1, double.MaxValue, ErrorMessage = "Debe introducir un monto valido")]
     public double Monto { get; set; }
 
-    public ICollection<CobrosDetalle> CobrosDetalle { get; set; } = new List<CobrosDetalle>();
+    [InverseProperty("Cobro")]
+    public virtual ICollection<CobrosDetalle> CobrosDetalle { get; set; } = new List<CobrosDetalle>();
 
     [ForeignKey("DeudorId")]
-    public virtual Deudores Deudor { get; set; }
+    [InverseProperty("Cobros")]
+    public virtual Deudores Deudor { get; set; } = null!;
 }
